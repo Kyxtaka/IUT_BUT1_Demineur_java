13c92d4fc84fb4ee48d2edcf0675a9fb0c26f071
Nathan Randriantsoa <134612654+Kyxtaka@users.noreply.github.com>
2024-03-15 16:17:19 +0100

    Creation de CaseIntelligente.java (#2)

--- src/CaseIntelligente.java
+++ src/CaseIntelligente.java
@@ -1,4 +1,27 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class CaseIntelligente extends Case {
+
+    private List<Case> lesVoisines;
     
-    
+    public CaseIntelligente() {
+        super();
+        this.lesVoisines = new ArrayList();
+    }
+
+    public void ajouteVoisine(Case uneCase) {
+        this.lesVoisines.add(uneCase)
+    }
+
+    public int nombreBombesVoisines() {
+        // a faire
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        // a faire
+        return " sfqfqsf "
+    }
 }
