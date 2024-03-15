9a3e8d5dd1076560db3d78fe545a3b5763c14859
Kyxtaka <randriantsoanathan1@gmail.com>
2024-03-15 15:46:59 +0100

    initialisation phase 2

---
+++ other/Pour_etu/fichiers projet/Plateau.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Plateau{
+
+
+    protected void poseDesBombesAleatoirement(){
+        Random generateur = new Random();
+        for (int x = 0; x < this.getNbLignes(); x++){
+            for (int y = 0; y < this.getNbColonnes(); y++){
+                if (generateur.nextInt(100)+1 < this.pourcentageDeBombes){
+                    this.poseBombe(x, y);
+                    this.nbBombes = this.nbBombes + 1;
+                }
+            }
+        }
+    }
+
+}
