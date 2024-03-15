51511696c7b138cdf9205724cb554e46a44e6bb9
Nathan Randriantsoa <134612654+Kyxtaka@users.noreply.github.com>
2024-03-15 16:07:18 +0100

    Ajout de Case et Case intelligente (#1)
    Ajout et Implementation de la class Case

---
+++ src/Case.java
@@ -0,0 +1,49 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Case {
+    private boolean contientUneBombe;
+    private boolean estDecouverte;
+    private boolean estMarquee;
+
+    public Case() {
+        this.contientUneBombe = false;
+        this.estDecouverte = false;
+        this.estMarquee = false;
+    }
+
+    public void reset() {
+        this.contientUneBombe = false;
+        this.estDecouverte = false;
+        this.estMarquee = false;
+    }
+
+    public void poseBombe() {
+        this.contientUneBombe = true;
+    }
+
+    public boolean contientUneBombe() {
+        return this.contientUneBombe;
+    }
+
+    public boolean estDecouverte() {
+        return this.estDecouverte;
+    }
+
+    public boolean estMarquee() {
+        return this.estMarquee;
+    }
+
+    public boolean reveler() {
+        if (!this.estDecouverte) {
+            this.estMarquee = false;
+            this.estDecouverte = true;
+            return true;
+        }
+        return false;
+    }
+
+    public void marquer() {
+        this.estMarquee = true;
+    }
+}
---
+++ src/CaseIntelligente.java
@@ -0,0 +1,4 @@
+public class CaseIntelligente extends Case {
+    
+    
+}
